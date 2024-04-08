190960a6fc6438806d478420d2a11183b4b0ba8e
PabloG <pawelgarbula@gmail.com>
2024-04-08 15:01:32 +0200

    add code

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+
+function galleryItemEl(arr) {
+  arr.map((item) => {
+    galleryEl.insertAdjacentHTML(
+      "afterbegin",
+      `<a class="gallery__item" href='${item.original}'>
+  <img class="gallery__image" src='${item.preview}' alt='${item.description}' />
+</a>`
+    );
+  });
+}
+
+galleryItemEl(galleryItems);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
